fe78e807a4f23a0d6e10629f4dfff8d97b1c1714
Ross Perkel <rossperkel@gmail.com>
2019-11-13 15:40:39 -0500

    refactor 11/13

--- JavascriptSolutions/index.js
+++ JavascriptSolutions/index.js
@@ -57,6 +57,52 @@ function to_nato(words) {
     )
     return newStr.trim()
 }
+
+
+// Refactor:
+function to_nato(words) {
+    const alphabet = {
+        'a': 'Alfa',
+        'b': 'Bravo',
+        'c': 'Charlie',
+        'd': 'Delta',
+        'e': 'Echo',
+        'f': 'Foxtrot',
+        'g': 'Golf',
+        'h': 'Hotel',
+        'i': 'India',
+        'j': 'Juliett',
+        'k': 'Kilo',
+        'l': 'Lima',
+        'm': 'Mike',
+        'n': 'November',
+        'o': 'Oscar',
+        'p': 'Papa',
+        'q': 'Quebec',
+        'r': 'Romeo',
+        's': 'Sierra',
+        't': 'Tango',
+        'u': 'Uniform',
+        'v': 'Victor',
+        'w': 'Whiskey',
+        'x': 'Xray',
+        'y': 'Yankee',
+        'z': 'Zulu',
+    }
+
+    let newStr = ""
+    words.replace(/\s+/gi, "").toLowerCase().split("").map(char => {
+        if (alphabet[char]) {
+            newStr += " " + alphabet[char]
+        }
+        else {
+            newStr += " " + char
+        }
+    }
+    )
+    return newStr.trim()
+}
+
 // Kata: https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
 // 6 kyu, Find the Odd Int
 
